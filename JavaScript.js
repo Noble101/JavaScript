e9753987859ac884da472ede8b59428c42d6a5e3
Sunday Emmanuel <105364216+Noble101@users.noreply.github.com>
2022-06-24 00:14:59 +0100

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+let m_name = "Sunday Emmanuel";
+let height = 1.6;
+let country = "Nigeria";
+console.log("My information is as follows:");
+console.log("Name:",m_name);
+console.log("Height:",height, "meters");
+console.log("Country:",country);
